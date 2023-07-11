be7d75f521cbe1458023561dc2522881d020502a
Egor Piankov <piankovegor@gmail.com>
2023-07-11 02:57:38 +0200

    Fixing bash scripts

--- scripts/prod/azure_create_db.sh
+++ scripts/prod/azure_create_db.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#az postgres flexible-server create \
-#  --resource-group MSAJDGroup \
-#  --name msajd-petstore \
-#  --location westeurope \
-#  --admin-user msajd \
-#  --admin-password paSSw0rd \
-#  --sku-name Standard_B1ms \
-#  --tier Burstable \
-#  --storage-size 32 \
-#  --version 14 \
-#  --yes
-#
-#sleep 30s
-#
-#az postgres flexible-server db create \
-#  --database-name petstore \
-#  --resource-group MSAJDGroup \
-#  --server-name msajd-petstore
+az postgres flexible-server create \
+  --resource-group MSAJDGroup \
+  --name msajd-petstore \
+  --location westeurope \
+  --admin-user msajd \
+  --admin-password paSSw0rd \
+  --sku-name Standard_B1ms \
+  --tier Burstable \
+  --storage-size 32 \
+  --version 14 \
+  --yes
+
+sleep 30s
+
+az postgres flexible-server db create \
+  --database-name petstore \
+  --resource-group MSAJDGroup \
+  --server-name msajd-petstore
 
 az cosmosdb create \
   --name msajd-cosmosdb \
--- scripts/prod/azure_create_function.sh
+++ scripts/prod/azure_create_function.sh
@@ -4,10 +4,34 @@ az storage account create \
   --resource-group MSAJDGroup \
   --location westeurope \
   --sku Standard_LRS \
-  --kind BlobStorage
+  --kind StorageV2 \
+  --access-tier Cool
+
+sleep 30s
+
+az storage container create \
+  --name msajdsa-blob \
+  --account-name msajdsa \
+  --public-access blob
+
+sleep 30s
 
 az functionapp create \
   --name msajd-order-reserver \
   --resource-group MSAJDGroup \
   --storage-account msajdsa \
-  --deployment-source-url "https://github.com/Evigilans/petstoreorderreserver"
+  --consumption-plan-location westeurope \
+  --functions-version 4 \
+  --deployment-source-url "https://github.com/Evigilans/petstoreorderreserver" \
+  --os-type Linux \
+  --runtime "java" \
+  --runtime-version "17.0"
+
+sleep 30s
+
+az functionapp deployment github-actions add \
+  --name msajd-order-reserver \
+  --resource-group MSAJDGroup \
+  --repo "https://github.com/Evigilans/petstoreorderreserver" \
+  --runtime "java" \
+  --runtime-version "17.0"
